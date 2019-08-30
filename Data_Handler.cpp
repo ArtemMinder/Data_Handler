daf348492de5cdafb6f176166a5fc1f9046f29bb
Artem Minder <a.minder@mail.ru>
2019-08-30 18:51:27 +0300

    indentation editing

--- Data_Handler.cpp
+++ Data_Handler.cpp
@@ -3,20 +3,20 @@
 #include "Data_Handler.h"
 
 void DataHandler::read() {
-for (int i = 0; i < info.size(); i++) {
-	std::cout << info[i] << std::endl;
-}
+	for (int i = 0; i < info.size(); i++) {
+		std::cout << info[i] << std::endl;
+	}
 }
 
 void  DataHandler::data(std::string const& name) {
-std::ifstream file(name);
-if (file.is_open()) {
-	while (file.good()) {
+	std::ifstream file(name);
+	if (file.is_open()) {
+		while (file.good()) {
 		std::string line;
 		getline(file, line, ',');
 		info.push_back(line);
+		}
+	} else {
+		std::cout << "File is not opened\n";
 	}
-} else {
-	std::cout << "File is not opened\n";
-}
 }
